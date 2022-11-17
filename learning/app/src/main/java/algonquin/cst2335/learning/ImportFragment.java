a59a8ce717a7c4677458a56c7a11ee44d999d9f3
PangVicky <chunxia.pang@outlook.com>
2022-11-17 17:40:54 -0500

    save partly

---
+++ learning/app/src/main/java/algonquin/cst2335/learning/ImportFragment.java
@@ -0,0 +1,23 @@
+package algonquin.cst2335.learning;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by velmmuru on 1/6/2018.
+ */
+
+public class ImportFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.import_fragment,container,false);
+    }
+}
